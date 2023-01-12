5ea17c309785663a0eacf89d975ca940aa5f81c8
Claude-Jhin <76059275+Claude-Jhin@users.noreply.github.com>
2023-01-12 20:15:17 +0800

    Update TextAssetEditorStyle.h

--- Source/TextAssetEditor/Private/Styles/TextAssetEditorStyle.h
+++ Source/TextAssetEditor/Private/Styles/TextAssetEditorStyle.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#include "IPluginManager.h"
 #include "Brushes/SlateImageBrush.h"
+#include "Interfaces/IPluginManager.h"
 #include "Styling/SlateStyle.h"
 #include "Styling/SlateStyleRegistry.h"
 
